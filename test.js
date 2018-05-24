7ce67d89d9c1b87ed83e96e2dc047b7f74403a5d
astur <astur@yandex.ru>
2018-05-24 17:52:14 +0300

    first tests

--- test.js
+++ test.js
@@ -1,7 +1,38 @@
 const test = require('ava');
-const m = require('.');
+const delay = require('delay');
+const whiler = require('.');
 
-test('whiler', t => {
-    t.true(true);
-    t.is(m, m);
+test('sync', async t => {
+    let i = 0;
+    await whiler(() => {
+        i++;
+        return i < 10;
+    });
+    t.is(i, 10);
+});
+
+test('async', async t => {
+    let i = 0;
+    await whiler(async () => {
+        i++;
+        await delay(10);
+        return i < 10;
+    });
+    t.is(i, 10);
+});
+
+test('throw', async t => {
+    const fixtureErr = new Error('fixture');
+    await whiler(() => {
+        throw fixtureErr;
+    }).then(() => t.fail()).catch(err => {
+        t.is(err, fixtureErr);
+    });
+});
+
+test('reject', async t => {
+    const fixtureErr = new Error('fixture');
+    await whiler(() => Promise.reject(fixtureErr)).then(() => t.fail()).catch(err => {
+        t.is(err, fixtureErr);
+    });
 });
